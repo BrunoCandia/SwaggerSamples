137d7e63b21b74d9c2d7399f7941313de5c5189f
Owner <brunocandia@gmail.com>
2018-08-23 13:49:20 -0300

    Initial commit

---
+++ SuperHeroGen/Controllers/HeroesApiController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroGen.Models;
+
+namespace SuperHeroGen.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]    
+    [Produces("application/json")]
+    public class HeroesApiController : ControllerBase
+    {
+        [HttpGet("{firstName}/{lastName}")]
+        public Person Get(string firstName, string lastName)
+        {
+            var person = new Person { FirstName = firstName, LastName = lastName };
+
+            //person.SetHeroName();
+
+            return person;
+        }
+    }
+}
